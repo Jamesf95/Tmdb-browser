72ddabe3ae301872667fba23580b6f648b051559
James Foot <jamesfoot@Jamess-MacBook-Air.local>
2025-07-22 18:01:57 +0100

    Initial commit

---
+++ TMDB Browser/TMDB_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDB_BrowserApp.swift
+//  TMDB Browser
+//
+//  Created by James Foot on 22/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct TMDB_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TMDB BrowserTests/TMDB_BrowserTests.swift
@@ -0,0 +1,16 @@
+//
+//  TMDB_BrowserTests.swift
+//  TMDB BrowserTests
+//
+//  Created by James Foot on 22/07/2025.
+//
+
+import Testing
+
+struct TMDB_BrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
